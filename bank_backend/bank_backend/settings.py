04977c668beeeddf1cf9343da3e2445829dbfe1e
MR. Morningstar <ee8991471@gmail.com>
2025-07-13 21:30:54 +0600

    postgresql  added

--- bank_backend/bank_backend/settings.py
+++ bank_backend/bank_backend/settings.py
@@ -81,11 +81,28 @@ WSGI_APPLICATION = 'bank_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'UqXRHXNQRetNRgJDhgZCmTRLWVkOPJYi',
+
+        'HOST': 'crossover.proxy.rlwy.net',
+
+        'PORT': '37689',
+
     }
+
 }
+# postgresql://postgres:UqXRHXNQRetNRgJDhgZCmTRLWVkOPJYi@crossover.proxy.rlwy.net:37689/railway
+
 
 
 # Password validation
